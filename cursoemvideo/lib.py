b308d9de5439325ee8487684b9fa6aef1b91d065
May <mayancavalcantis@gmail.com>
2024-01-15 22:26:56 -0400

    library

--- cursoemvideo/lib.py
+++ cursoemvideo/lib.py
@@ -1,6 +1,15 @@
 #### Library ####
 
+# Importando toda a biblioteca
+
 import math
 num = int(input('Digite um número: '))
 raiz = math.sqrt(num)
 print(f'A raiz quadrada de {num} é {math.ceil(raiz)}')
+
+# Importando uma função específica
+
+from math import sqrt
+num = int(input('Digite um número: '))
+raiz = sqrt(num)
+print(f'A raiz quadrada de {num} é {raiz :.3f}')

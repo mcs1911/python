976c7f2e40b3b98f0170a424f8cb469acf6feb2c
May <mayancavalcantis@gmail.com>
2024-01-17 18:30:30 -0400

    atualização

--- cursoemvideo/ex026.py
+++ cursoemvideo/ex026.py
@@ -1,4 +1,4 @@
-frase = str(input('Digite uma frase: ')).lower()
+frase = str(input('Digite uma frase: ')).lower().strip()
 print(f' A letra a aparece {frase.count('a')} vezes na sua frase')
 print(f'A primeira letra a aparece na posição {frase.find('a')+1}')
 print(f'A última letra a aparece na posição {frase.rfind('a')+1}')

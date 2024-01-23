6752d3d53a39f3af6402d4dc94fca2ce2e69ff1d
May <mayancavalcantis@gmail.com>
2024-01-23 07:39:00 -0400

    ajustes

---
+++ cursoemvideo/ex053.py
@@ -0,0 +1,20 @@
+frase = str(input('Digite uma frase: ')).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+for letra in range(len(junto) -1, -1, -1):
+    inverso += junto[letra]
+print(f'O inverso de {junto} é {inverso}')
+if inverso == junto:
+    print('Temos um PALÍNDROMO!')
+else: 
+    print('A frase digitada NÃO É UM PALÍNDROMO!')
+
+# Sem o for teria que colocar
+# inverso = junto[::-1]
+
+'''frase = input("Qual a frase? ").upper().replace(" ", "")
+if frase == frase[::-1]:
+    print("A frase é um palíndromo")
+else:
+    print("A frase não é um palíndromo")'''

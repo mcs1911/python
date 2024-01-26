2cfafba33c8ea81f367d38204874cec6d50a5723
May <mayancavalcantis@gmail.com>
2024-01-26 11:58:56 -0400

    exercícios while

--- cursoemvideo/ex057.py
+++ cursoemvideo/ex057.py
@@ -1,11 +1,14 @@
 print('-=-' * 20)
 print('Inicio')
 
-M = 'Masculino'
-F = 'Feminino'
+s = 'P'
 while s != 'M' and s != 'F':
     s = str(input('Sexo [M/F]: ')).upper()
     print('Por favor digite um sexo válido!')
+    if s == 'M':
+        print(f'Você digitou Masculino')
+    if s == 'F':
+        print(f'Você digitou Feminino')
 print(f'Você digitou {s}')
 
 
---
+++ cursoemvideo/ex058.py
@@ -0,0 +1,17 @@
+from random import randint
+
+print('-=-' * 20)
+print('JOGO DE ADIVINHAÇÃO')
+print('-=-' * 20)
+print('''Regra do jogo: O computador vai pensar em um número de 0 a 10 e você precisa adivinhar em qual número ele pensou...''')
+print('-=-' * 20)
+
+comp = randint(0, 10)
+tentativas = 0 
+jogador = int(input('Em qual número o computador pensou? '))
+while comp != jogador:
+    print('Você \033[31mErrou!!\033[mTente novamente!')
+    jogador = int(input('Tente outro número: '))
+    tentativas += 1
+print(f'\033[34mVocê ACERTOU!\033[m O número escolhido pelo computador foi {comp}')
+print(f'Você precisou de \033[33m{tentativas}\033[m tentativas!')

219bef5737ae415b7198d7b7762fd670539a7d9d
May <mayancavalcantis@gmail.com>
2024-01-26 13:56:18 -0400

    ex

--- cursoemvideo/ex060.py
+++ cursoemvideo/ex060.py
@@ -1,7 +1,10 @@
 print('FATORIAL!!')
 
-num = 1
-n = num
-while num != 0:
-    n -= 1
-    print(num * n)
+num = 2
+n = num - 1
+fat = num * (n - 1) * (n -2)
+num = int(input('Digite um número: '))
+while n != 0:
+    n = num - 1
+
+print(f'{num}! é = {fat}')
---
+++ cursoemvideo/ex064.py
@@ -0,0 +1,8 @@
+print('-=-' * 20)
+
+n = 0
+soma = 0
+while n != 999:
+    n = int(input('Digite um número inteiro qualquer: '))
+    soma = soma + n
+print(f'A soma de todos os números digitados foi {soma}')

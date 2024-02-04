81572cc2d6dd1776a1e9d95b117050cc689abde0
May <mayancavalcantis@gmail.com>
2024-02-04 13:58:51 -0400

    for

---
+++ exercicios/testes.py
@@ -0,0 +1,20 @@
+'''
+for j in range(5, -1, -1):
+    print(j)
+print("Fim do laço 2.")
+
+n = int(input("Digite um número: \n"))
+for numero in range(0, n + 1):
+    print(numero)
+print("Fim do laço 3.")
+
+
+inicio = int(input("Início: \n"))
+fim = int(input("Fim: \n"))
+passo = int(input("Passo: \n"))
+
+for m in range(inicio, fim + 1, passo):
+    print(m)
+print("fim do laço 4")
+
+'''

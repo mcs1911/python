8a17225d8f5d04d35c871e696b9731767509eb2f
May <mayancavalcantis@gmail.com>
2024-03-05 18:07:33 -0400

    exercicios

--- cursoemvideo/ex087.py
+++ cursoemvideo/ex087.py
@@ -8,7 +8,7 @@ c: o maior valor da segunda linha
 '''
 
 matriz = []
-soma = 0
+soma = terceira = maior = 0
 
 for r in range(3):
     linha = list()
@@ -17,10 +17,36 @@ for r in range(3):
         linha.append(n)
         if n % 2 == 0:
             soma += n
+        '''
+        if n in linha[r][3]:
+            terceira += n
+        '''   
     matriz.append(linha)
 
+for i in matriz[1]:
+    if i == 0:
+        maior = i
+    else:
+        if i > maior:
+            maior = i
+
+            
 for linha in matriz:
     print(linha)
 print()
 
 print(f'A soma de todos os valores pares digitados é: {soma}')
+print(f'A soma dos valores da terceira coluna é: {terceira}')
+print(f'O maior valor da segunda linha é: {maior}')
+
+#print(matriz[0][2])
+
+'''
+if n[r][3]:
+    terceira += n
+if n[2][c]:
+    maior = n[2][0]
+    if n > maior:
+        maior = n
+maior = max(linha[1])
+'''
